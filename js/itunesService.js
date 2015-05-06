0d7e74d1270df589a904c98e99b45df9d1b4d400
EricScottRichards <ericscottrichards@gmail.com>
2015-05-05 20:40:37 -0600

    Almost there.  Just need to massage the data.

--- js/itunesService.js
+++ js/itunesService.js
@@ -9,5 +9,20 @@ app.service('itunesService', function($http, $q){
   //Note that in the above line, artist is the parameter being passed in. 
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
-    //Code here
+  var artist;
+
+  this.sMethod = function(name) {
+
+  	artist = name
+  	var deferred = $q.defer()
+  	$http({
+  		method: 'JSONP',
+  		url: 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK',
+		}).then(function(response){
+			console.log(response)
+			deferred.resolve(response.data.results)
+		})
+  	return deferred.promise;
+  }
+
 });
