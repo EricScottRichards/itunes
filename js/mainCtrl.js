b68aa29a87b1a0906ad11131c83f6e86342d04d0
EricScottRichards <ericscottrichards@gmail.com>
2015-05-06 17:44:56 -0600

    Got filter working but it's uni universal. I wanted one input field per column...

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -4,19 +4,30 @@ app.controller('mainCtrl', function($scope, itunesService){
   //This is setting up the default behavior of our ng-grid. The important thing to note is
   //the 'data' property. The value is 'songData'. That means ng-grid is looking for songData on $scope and is putting whatever songData is into the grid.
   //this means when you make your iTunes request, you'll need to get back the information, parse it accordingly, then set it to songData on the scope -> $scope.songData = ...
+  
+  $scope.filterOptions = {
+    filterText: ''
+        // filterArtist: '',
+        // filterSong: '',
+        // filterCollection: '',
+        // filterCollectionPrice: ''
+  };
+
   $scope.gridOptions = { 
-      data: 'songData',
-      height: '110px',
-      sortInfo: {fields: ['Song', 'Artist', 'Collection', 'Type'], directions: ['asc']},
-      columnDefs: [
-        {field: 'Play', displayName: 'Play', width: '40px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><a href="{{row.getProperty(col.field)}}"><img src="http://www.icty.org/x/image/Miscellaneous/play_icon30x30.png"></a></div>'},
-        {field: 'Artist', displayName: 'Artist'},
-        {field: 'Song', displayName: 'Song'},
-        {field: 'Collection', displayName: 'Collection'},
-        {field: 'AlbumArt', displayName: 'Album Art', width: '110px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><img src="{{row.getProperty(col.field)}}"></div>'},
-        {field: 'Type', displayName: 'Type'},
-        {field: 'CollectionPrice', displayName: 'Collection Price'},
-      ]
+    data: 'songData',
+    filterOptions: $scope.filterOptions,
+    height: '110px',
+    sortInfo: {fields: ['Song', 'Artist', 'Collection', 'Type'], directions: ['asc']},
+    columnDefs: [
+      {field: 'Play', displayName: 'Play', width: '40px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><a href="{{row.getProperty(col.field)}}"><img src="http://www.icty.org/x/image/Miscellaneous/play_icon30x30.png"></a></div>'},
+      {field: 'Artist', displayName: 'Artist'},
+      {field: 'Song', displayName: 'Song'},
+      {field: 'Collection', displayName: 'Collection'},
+      {field: 'AlbumArt', displayName: 'Album Art', width: '110px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><img src="{{row.getProperty(col.field)}}"></div>'},
+      {field: 'Type', displayName: 'Type'},
+      {field: 'CollectionPrice', displayName: 'Collection Price'},
+    ]
+
   };
 
   //Our controller is what's going to connect our 'heavy lifting' itunesService with our view (index.html) so our user can see the results they get back from itunes.
@@ -50,6 +61,7 @@ app.controller('mainCtrl', function($scope, itunesService){
           entry = null
         }
         $scope.songData = finalArray
+        $scope.show = true
 
       })
 
